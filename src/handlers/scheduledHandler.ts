4a141b9eb2ed6833278f884ce2fed35f10ab277c
sena-m09 <sena-murakami@gaji.jp>
2025-04-29 22:21:14 +0900

    feat: cron でテスト送信実装

--- src/handlers/scheduledHandler.ts
+++ src/handlers/scheduledHandler.ts
@@ -2,13 +2,12 @@ import { ExportedHandlerScheduledHandler } from "@cloudflare/workers-types";
 import { sendPushMessage } from "../services/lineClient";
 import { Bindings } from "../types";
 
-export const handleScheduled: ExportedHandlerScheduledHandler = async (event, env, ctx) => {
+export const handleScheduled: ExportedHandlerScheduledHandler<Bindings> = async (event, env, ctx) => {
   // スケジュール実行時に何かアクションを起こしたい場合はここに実装
   // 例: 定期的なメッセージ送信
-  // ctx.waitUntil(
-  //   sendPushMessage(env as Bindings, "定期実行メッセージです")
-  //     .catch(error => console.error("スケジュール送信エラー:", error))
-  // );
+  ctx.waitUntil(
+    sendPushMessage(env, "テスト")
+      .catch(error => console.error("スケジュール送信エラー:", error))
+  );
   
-  ctx.waitUntil(console.log("スケジュールタスク実行:", new Date().toISOString()));
 }; 
