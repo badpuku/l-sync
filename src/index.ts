426aea0f21fadf9ac5feb0214ab15822231b5368
sena-m09 <sena-murakami@gaji.jp>
2025-04-29 16:20:18 +0900

    feat: push message のテスト

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as line from "@line/bot-sdk";
 type Bindings = {
   LINE_CHANNEL_ACCESS_TOKEN: string;
   LINE_CHANNEL_SECRET: string;
+  LINE_GROUP_ID: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>()
@@ -36,6 +37,8 @@ const textEventHandler = async (
   await client.replyMessage(replyMessageRequest);
 };
 
+// LINE Webhookのエンドポイント
+// LINE で受け取ったメッセージをそのまま返し、groupIDを取得する
 app.post('/webhook', async (c) => {
   const config: line.ClientConfig = {
     channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
@@ -61,6 +64,38 @@ app.post('/webhook', async (c) => {
   return c.status(200);
 });
 
+app.post("/api/v1/line/push", async (c) => {
+  try {
+    const text = "push message のテスト";
 
+    const res = await fetch('https://api.line.me/v2/bot/message/push', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${c.env.LINE_CHANNEL_ACCESS_TOKEN}`
+      },
+      body: JSON.stringify({
+        to: c.env.LINE_GROUP_ID,
+        messages: [
+          {
+            type: 'text',
+            text: text
+          }
+        ]
+      })
+    })
+
+    if (!res.ok) {
+      const error = await res.json()
+      console.error('LINE API error:', error)
+      return c.json({ status: 'LINE API error', message: error }, 500)
+    }
+
+    return c.json({ status: 'success' }, 200)
+  } catch (error) {
+    console.error('Internal server error:', error)
+    return c.json({ status: 'error', message: error }, 500)
+  }
+})
 
 export default app
