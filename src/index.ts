a81d33cb2a9364bd44fbbd2c3428e3f478b1a88b
sena-m09 <sena-murakami@gaji.jp>
2025-04-29 15:21:27 +0900

    feat: Line のメッセージをおうむ返しするよう変更

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,64 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import * as line from "@line/bot-sdk";
 
-const app = new Hono()
+type Bindings = {
+  LINE_CHANNEL_ACCESS_TOKEN: string;
+  LINE_CHANNEL_SECRET: string;
+};
 
+const app = new Hono<{ Bindings: Bindings }>()
+
+// ルートパスへのハンドラを追加
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('LINE Botサーバーが正常に動作しています');
+});
+
+const textEventHandler = async (
+  client: line.messagingApi.MessagingApiClient,
+  event: line.WebhookEvent,
+): Promise<line.MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+
+  const { replyToken, message: { text } = {} } = event;
+
+  const response: line.TextMessage = {
+    type: 'text',
+    text: `${text}と言われましても`,
+  };
+
+  const replyMessageRequest: line.messagingApi.ReplyMessageRequest = {
+    replyToken: replyToken,
+    messages: [response],
+  };
+
+  await client.replyMessage(replyMessageRequest);
+};
+
+app.post('/webhook', async (c) => {
+  const config: line.ClientConfig = {
+    channelAccessToken: c.env.LINE_CHANNEL_ACCESS_TOKEN,
+  };
+  const client = new line.messagingApi.MessagingApiClient(config);
+  line.middleware({ channelSecret: c.env.LINE_CHANNEL_SECRET });
+
+  const events: line.WebhookEvent[] = await c.req.json().then((data) => data.events);
+
+  await Promise.all(
+    events.map(async (event: line.WebhookEvent) => {
+      try {
+        await textEventHandler(client, event);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(err);
+        }
+        return c.status(500);
+      }
+    }),
+  );
+
+  return c.status(200);
+});
 
 export default app
